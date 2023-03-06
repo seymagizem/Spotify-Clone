002de414f0e67251ad2ed6037c87b18da24367d1
Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-03-06 15:44:59 +0300

    Add files via upload

---
+++ spotify-home-clone/data/Data.js
@@ -0,0 +1,125 @@
+export const trendingData = [
+{
+    title:"Starboy",
+    artists:"The Weekend",
+    img:"https://i.pinimg.com/564x/87/be/1f/87be1f0ace91339accec0071b38373b6.jpg"
+},
+{
+    title:"1989",
+    artists:"Taylor Swift",
+    img:"https://i.pinimg.com/564x/11/29/1d/11291de37f3ed6bf58bb2cfe8370fb5b.jpg"
+},
+{
+    title:"Back To Black",
+    artists:"Amy Winhouse",
+    img:"https://i.pinimg.com/564x/c2/ce/0b/c2ce0b2f6e99241b4abe8d2770e71b92.jpg"
+},
+{
+    title:"21",
+    artists:"Adele",
+    img:"https://i.pinimg.com/564x/7e/95/08/7e9508f5b1a75ebe8436e95dfecd394f.jpg"
+},
+{
+    title:"Havana",
+    artists:"Camila Cabello",
+    img:"https://i.pinimg.com/564x/82/ba/50/82ba5034f0572fabbfa420e888a83339.jpg"
+},
+{
+    title:"Silk Sonic",
+    artists:"Bruno Mars",
+    img:"https://i.pinimg.com/564x/23/57/fc/2357fc6dd2dc86470b52590f4501c9af.jpg"
+},
+]
+
+export const showsData =[
+    {
+        title:"Evolve",
+        artists:"Imagine Dragons",
+        img:"https://i.pinimg.com/564x/9b/d7/c9/9bd7c97d85cd96ee281adb8dbd0d2f3e.jpg"
+    },
+    {
+        title:"Joanne",
+        artists:"Lady Gaga",
+        img:"https://i.pinimg.com/564x/3c/28/ac/3c28accc644c1d681e135788ad218a6b.jpg"
+    },
+    {
+        title:"Harry's House",
+        artists:"Harry Styles",
+        img:"https://i.pinimg.com/564x/64/bb/42/64bb428a3f62d105cf8bdf082c5af99c.jpg"
+    },
+    {
+        title:"Cactus Jack",
+        artists:"Travis Scott",
+        img:"https://i.pinimg.com/564x/e3/b4/a5/e3b4a57f8a562021f576ae29d9780a44.jpg"
+    },
+    {
+        title:"Stressed Out",
+        artists:"Twenty One Pilot",
+        img:"https://i.pinimg.com/564x/de/3d/ba/de3dbab56f15830aedb17471e7863a2d.jpg"
+    },
+    {
+        title:"Born to die",
+        artists:"Lana Del Rey",
+        img:"https://i.pinimg.com/564x/a6/94/cd/a694cdc898240a27598fd063ed9c48c9.jpg"
+    },
+]
+
+export const mixData = [
+    {
+        title:"Daily Mix 1",
+        artists:"Copene",
+        img:"https://i.pinimg.com/564x/2b/eb/de/2bebdefe97d027a9f172c7adc71793aa.jpg"
+    },
+    {
+        title:"Daily Mix 3",
+        artists:"Billie Eilish ",
+        img:"https://i.pinimg.com/564x/d9/f6/8e/d9f68e027b322fbba4886009fe64a311.jpg"
+    },
+    {
+        title:"Daily Mix 4",
+        artists:"Remixtd",
+        img:"https://i.pinimg.com/564x/37/bd/42/37bd427520fac831d5d89b065b3eb443.jpg"
+    },
+    {
+        title:"The Weekend Mix",
+        artists:"The Weekend",
+        img:"https://i.pinimg.com/564x/6d/c9/09/6dc9096eb3c2a33a19d27f26e90dcb3b.jpg"
+    },
+    {
+        title:"2000's Mix",
+        artists:"50 Cent",
+        img:"https://i.pinimg.com/564x/12/95/0f/12950f4d9f4fddae4fd3e892c71c0265.jpg"
+    },
+    {
+        title:"Daily Mix 6",
+        artists:"Frank Ocean",
+        img:"https://i.pinimg.com/564x/ec/12/bc/ec12bc180cda05c73f404dd51ad09d43.jpg"
+    },
+]
+
+export const cardData = [
+    {
+        title:"Kpapo",
+        img:"https://i.pinimg.com/564x/fe/2f/ce/fe2fce125d7aa46c905e2c30d47731be.jpg"
+    },
+    {
+        title:"Heavyweight",
+        img:"https://i.pinimg.com/564x/a6/3b/93/a63b93ef57c8c932a127d42baa6f0cef.jpg"
+    },
+    {
+        title:"The drop out ",
+        img:"https://i.pinimg.com/564x/08/33/55/0833559ef34fe0b247b64473852ed9cc.jpg"
+    },
+    {
+        title:"The Glow up",
+        img:"https://i.pinimg.com/564x/93/cb/1d/93cb1d4d14854a9db1d853a58b40ea95.jpg"
+    },
+    {
+        title:"Dear Diary",
+        img:"https://i.pinimg.com/564x/7f/cc/d6/7fccd6b54a908bb7c83f8a340b277e3c.jpg"
+    },
+    {
+        title:"The last archive",
+        img:"https://i.pinimg.com/736x/54/3c/dd/543cdd07cc3a37beeb1b81ccbd737004.jpg"
+    },
+]
